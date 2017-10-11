4046c04e8ca6be4bc69116342657e0d73676d5f5
Nicole Simone <nicole.simone@d2l.com>
2017-10-11 13:49:47 -0400

    Add dynamo section

---
+++ 3-dynamo/addUser.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const aws = require('aws-sdk');
+
+module.exports.handler = (event, context, callback) => {
+  const dynamodb = new aws.DynamoDB();
+
+  const name = event.name;
+  const id = event.id;
+
+  if (name && id) {
+    const params = {
+      Item: {
+        'Id': { S: id },
+        'Name': { S: name }
+      },
+      TableName: process.env.TABLE
+    };
+    dynamodb.putItem(params).promise()
+      .then(data => callback(null, data))
+      .catch(err => callback(err));
+  } else {
+    callback('Please provide a user and a user ID.');
+  }
+};

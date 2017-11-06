f03c47ece89d058fe4fc0228fa4cedbf5634c24c
Nicole Simone <nicole.simone@d2l.com>
2017-11-06 15:57:03 -0500

    Update dynamo readme and code stubs

--- 3-dynamo-start/addUser.js
+++ 3-dynamo-start/addUser.js
@@ -1,6 +1,10 @@
 'use strict';
 
+const aws = require('aws-sdk');
+
 module.exports.handler = (event, context, callback) => {
+  const dynamodb = new aws.DynamoDB();
+
   const name = event.name;
   const id = event.id;
 
